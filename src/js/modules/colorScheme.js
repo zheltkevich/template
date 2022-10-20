8aeb6777e0a59c6da108a31b3cb119e51c4bfc79
Aleksandr <zheltkevich42@gmail.com>
2022-10-21 00:59:38 +0300

    Update color schemes

--- src/js/modules/colorScheme.js
+++ src/js/modules/colorScheme.js
@@ -1,6 +1,24 @@
 const switcher = document.querySelector('#theme-switcher');
+const meta = document.querySelector('meta[name="theme-color"]');
 const doc = document.firstElementChild;
-const setTheme = theme => doc.setAttribute('color-scheme', theme);
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const prefersLightScheme = window.matchMedia('(prefers-color-scheme: light)');
 
-switcher.addEventListener('input', e => setTheme(e.target.value));
+if (prefersLightScheme.matches) meta.setAttribute('content', '#ffffff');
+else if (prefersDarkScheme.matches) meta.setAttribute('content', '#000000');
 
+const setTheme = theme => {
+    if (theme === 'auto') {
+        doc.removeAttribute('color-scheme');
+        meta.setAttribute('content', 'transparent');
+
+    } else {
+        doc.setAttribute('color-scheme', theme);
+        meta.setAttribute('content', theme);
+    }
+
+};
+
+switcher.addEventListener('input', e => {
+    setTheme(e.target.value);
+});

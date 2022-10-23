3a2be183fab5ee0531c75056b1e5268a0ea34dbb
Aleksandr <zheltkevich42@gmail.com>
2022-10-23 11:59:59 +0300

    Fix color scheme users choise

--- src/js/modules/colorScheme.js
+++ src/js/modules/colorScheme.js
@@ -5,9 +5,22 @@ const prefersLightScheme = matchMedia('(prefers-color-scheme: light)');
 const prefersDarkScheme = matchMedia('(prefers-color-scheme: dark)');
 const getSavedScheme = () => localStorage.getItem('color-scheme');
 
-if (prefersLightScheme.matches) meta.setAttribute('content', 'hsl(200 25% 90%)');
-else if (prefersDarkScheme.matches) meta.setAttribute('content', 'hsl(200 10% 10%)');
+const colors = {
+    light: 'hsl(200 25% 90%)',
+    dark: 'hsl(200 10% 10%)',
+    dim: 'hsl(200 10% 20%)',
+};
 
+const setTopCurtainColor = () => {
+    const savedScheme = getSavedScheme();
+
+    if (savedScheme === null) {
+        if (prefersLightScheme.matches) meta.setAttribute('content', colors.light);
+        else if (prefersDarkScheme.matches) meta.setAttribute('content', colors.dark);
+    } else {
+        meta.setAttribute('content', colors[savedScheme]);
+    }
+};
 const clearScheme = () => {
     localStorage.removeItem('color-scheme');
     html.removeAttribute('color-scheme');
@@ -19,6 +32,24 @@ const setScheme = scheme => {
         localStorage.setItem('color-scheme', scheme);
         html.setAttribute('color-scheme', scheme);
     }
+
+    setTopCurtainColor();
+};
+const setupSwitcher = () => {
+    const savedScheme = getSavedScheme();
+    let currentRadio = null;
+
+    if (savedScheme === null) currentRadio = document.querySelector('.scheme-switcher__input[value=auto]');
+    else currentRadio = document.querySelector(`.scheme-switcher__input[value=${savedScheme}]`);
+
+    currentRadio.checked = true;
+};
+const setupScheme = () => {
+    const savedScheme = getSavedScheme();
+
+    if (savedScheme === null) return;
+
+    setScheme(savedScheme);
 };
 
 schemeSwitcher.addEventListener('input', event => {
@@ -27,24 +58,10 @@ schemeSwitcher.addEventListener('input', event => {
     setScheme(value);
 });
 
-const setupSwitcher = () => {
-    const savedScheme = getSavedScheme();
-
-    if (savedScheme !== null) {
-        const currentRadio = document.querySelector(`.scheme-switcher__input[value=${savedScheme}]`);
-
-        currentRadio.checked = true;
-    }
-};
-const setupScheme = () => {
-    const savedScheme = getSavedScheme();
-
-    if (savedScheme !== null) setScheme(savedScheme);
-};
-
 const initColorScheme = () => {
-    setupSwitcher();
     setupScheme();
+    setupSwitcher();
+    setTopCurtainColor();
 };
 
 export default initColorScheme;

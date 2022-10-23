0c9f8ee0100dccf1c9599d58930e8bf2f3f5aaf4
Aleksandr <zheltkevich42@gmail.com>
2022-10-23 15:21:14 +0300

    Update color schemes

--- src/js/modules/colorScheme.js
+++ src/js/modules/colorScheme.js
@@ -5,26 +5,28 @@ const schemeSwitcher = document.querySelector('#scheme-switcher');
 const prefersLightScheme = matchMedia('(prefers-color-scheme: light)');
 const prefersDarkScheme = matchMedia('(prefers-color-scheme: dark)');
 
-
 const colors = {
     light: 'hsl(200 25% 90%)',
     dark: 'hsl(200 10% 10%)',
     dim: 'hsl(200 10% 20%)',
 };
 
-const setTopCurtainColor = scheme => {
-    if (scheme === 'auto') {
-        if (prefersLightScheme.matches) meta.setAttribute('content', colors.light);
-        else if (prefersDarkScheme.matches) meta.setAttribute('content', colors.dark);
-    } else {
-        meta.setAttribute('content', colors[scheme]);
-    }
-};
+if (prefersLightScheme.matches) meta.setAttribute('content', colors.light);
+else if (prefersDarkScheme.matches) meta.setAttribute('content', colors.dark);
+
+// const setTopCurtainColor = scheme => {
+//     if (scheme === 'auto') {
+//         if (prefersLightScheme.matches) meta.setAttribute('content', colors.light);
+//         else if (prefersDarkScheme.matches) meta.setAttribute('content', colors.dark);
+//     } else {
+//         meta.setAttribute('content', colors[scheme]);
+//     }
+// };
 
 const setScheme = scheme => {
     localStorage.setItem('color-scheme', scheme);
     html.setAttribute('color-scheme', scheme);
-    setTopCurtainColor(scheme);
+    // setTopCurtainColor(scheme);
 };
 
 const toggleLabelPickedClass = value => {
